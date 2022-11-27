ad9b7a65a771ef2db63515d6d4d8168fcd9207e5
VeliaSagita <velia.sagita.tif17@polban.ac.id>
2022-11-27 12:54:51 +0700

    displaying image

--- lib/views/tanaman.dart
+++ lib/views/tanaman.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable, avoid_print
+// ignore_for_file: must_be_immutable
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -7,6 +7,9 @@ import 'package:flutter/material.dart';
 import 'detail.dart';
 
 class Tanaman extends StatelessWidget {
+  final db = FirebaseFirestore.instance;
+  final storage = FirebaseStorage.instance;
+
   String namaKategori;
   String id;
 
@@ -14,8 +17,6 @@ class Tanaman extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    final storage = FirebaseStorage.instance;
     return Scaffold(
       body: StreamBuilder<QuerySnapshot>(
           stream: db
@@ -44,14 +45,27 @@ class Tanaman extends StatelessWidget {
                                 decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(10),
                                     color: Colors.grey.shade300),
-                                child: Center(
-                                  child: Text(
-                                      doc.get('nama') ?? doc.get('nama'),
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18),
-                                      textAlign: TextAlign.center),
-                                )),
+                                child: Stack(children: [
+                                  Image.network(
+                                    doc.get('img'),
+                                    fit: BoxFit.cover,
+                                    height:
+                                        MediaQuery.of(context).size.height / 4,
+                                    width:
+                                        MediaQuery.of(context).size.width / 3,
+                                    opacity:
+                                        const AlwaysStoppedAnimation<double>(
+                                            0.5),
+                                  ),
+                                  Center(
+                                    child: Text(
+                                        doc.get('nama') ?? doc.get('nama'),
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18),
+                                        textAlign: TextAlign.center),
+                                  ),
+                                ])),
                             onTap: () {
                               Navigator.push(
                                   context,
